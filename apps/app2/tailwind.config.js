0cb1c0737f4ca350ef185e8ea7d2b11a932f625e
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-10-07 17:03:25 +0100

    add second app with @angular-devkit builders

--- apps/app2/tailwind.config.js
+++ apps/app2/tailwind.config.js
@@ -3,7 +3,7 @@ const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 module.exports = {
   mode: 'jit',
   purge: [
-    './apps/app1/**/*.{html,ts}',
+    './apps/app2/**/*.{html,ts}',
     ...createGlobPatternsForDependencies(__dirname),
   ],
   darkMode: false, // or 'media' or 'class'
